64aacfedd1ae3cdc5a140f29954720855a86b658
laurameireles23 <laura.meireles1@gmail.com>
2020-05-12 21:20:23 -0300

    Create route transactions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace 'api' do
   	namespace 'v1' do
       resources :accounts, only: %i[create show], param: :number
+
+      resources :transactions, only: %i[create]
   	end
   end
 end
