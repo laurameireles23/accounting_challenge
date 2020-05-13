e00232b60d91b2305b359031ccb2bcf2825a9988
laurameireles23 <laura.meireles1@gmail.com>
2020-05-12 23:14:39 -0300

    Update transaction

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,9 +1,10 @@
 require  'securerandom'
 
 FactoryBot.define do
-  factory :transactions do
+  factory :transaction do
     source_account_id { SecureRandom.random_number(9999) }
     destination_account_id { SecureRandom.random_number(9999) }
     amount { 50000 }
+    not_approved { true }
   end
 end
