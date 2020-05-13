b11d1c0c5fcb0a944be61b4efa188ce47d183461
laurameireles23 <laura.meireles1@gmail.com>
2020-05-13 00:44:37 -0300

    Fixing bug

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,32 +1,32 @@
+# frozen_string_literal: true
+
 module Api::V1
   class TransactionsController < ApplicationController
     before_action :authenticate
 
     def create
-      begin
-        transaction = Transaction.create!(transaction_params)
-        source_account = Account.find_by!(number: transaction[:source_account_id])
-        destination_account = Account.find_by!(number: transaction[:destination_account_id])
-        amount = transaction.amount 
-        
-        if source_account.balance < amount
-          message = 'O saldo não é suficiente.'
-          status = :bad_request
-        end
+      transaction = Transaction.create!(transaction_params)
+      source_account = Account.find_by!(number: transaction[:source_account_id])
+      destination_account = Account.find_by!(number: transaction[:destination_account_id])
+      amount = transaction.amount
 
+      if source_account.balance < amount
+        message = 'O saldo não é suficiente.'
+        status_render = :bad_request
+      else
         source(source_account, amount)
         destination(destination_account, amount)
         transaction.update!(not_approved: false)
-
+  
         message = 'Transação realizada com sucesso!'
-        status = :ok
-        render(message, status)
-        
-      rescue ActiveRecord::RecordInvalid
-        render json: {
-          message: "Falha ao criar a transação. Tente novamente!"
-        }, status: :unprocessable_entity
+        status_render = :ok
       end
+
+      render_json(message, status_render)
+    rescue ActiveRecord::RecordInvalid
+      render json: {
+        message: 'Falha ao criar a transação. Tente novamente!'
+      }, status: :unprocessable_entity
     end
 
     private
@@ -46,15 +46,15 @@ module Api::V1
     end
 
     def authenticate
-      authenticate_with_http_token do |token, option|
+      authenticate_with_http_token do |token, _option|
         Account.find_by!(token: token).present?
       end
     end
 
-    def render(message, status)
+    def render_json(message, status_render)
       render json: {
         message: message
-      }, status: status
+      }, status: status_render
     end
   end
 end
