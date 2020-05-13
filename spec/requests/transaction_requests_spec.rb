40f7a2ec34ad42688477bda6269308a418ad91e7
laurameireles23 <laura.meireles1@gmail.com>
2020-05-13 00:18:40 -0300

    Tidying up and adding more tests

--- spec/requests/transaction_requests_spec.rb
+++ spec/requests/transaction_requests_spec.rb
@@ -2,19 +2,26 @@ require 'rails_helper'
 
 describe 'POST /api/v1/transactions' do
   context 'when the correct parameters are passed' do
-    it 'returns 200 (ok status)' do
+    it 'returns 200 (ok status)', :aggregate_failures do
       first_account = create(:account, number: 1234)
       second_account = create(:account, number: 5678)
 
       params = {
         source_account_id: first_account.number,
         destination_account_id: second_account.number,
-        amount: 50000
+        amount: 50000,
+        not_approved: true
       }
 
-      post '/api/v1/transactions', params: params
+      post '/api/v1/transactions', 
+        headers: { 'HTTP_AUTHORIZATION' => first_account.token },
+        params: params
 
       expect(response).to have_http_status(:ok)
+      transaction = Transaction.last
+
+      expect(response).to have_http_status(:ok)
+      expect(transaction.not_approved).to eq false
     end
   end
 
@@ -28,13 +35,32 @@ describe 'POST /api/v1/transactions' do
         amount: 50000
       }
 
-      post '/api/v1/transactions', params: params
+      post '/api/v1/transactions',
+      headers: { 'HTTP_AUTHORIZATION' => second_account.token },
+      params: params
 
       expect(response).to have_http_status(:unprocessable_entity)
     end
+
+    it 'returns 400 (bad request status)', :aggregate_failures do
+      first_account = create(:account, number: 1234)
+      second_account = create(:account, number: 5678)
+
+      params = {
+        source_account_id: first_account.number,
+        destination_account_id: second_account.number,
+        amount: 160000,
+        not_approved: true
+      }
+
+      post '/api/v1/transactions',
+      headers: { 'HTTP_AUTHORIZATION' => first_account.token },
+      params: params
+
+      transaction = Transaction.last
+
+      expect(response).to have_http_status(:bad_request)
+      expect(transaction.not_approved).to eq true
+    end
   end
 end
-
-        #source_account_id: SecureRandom.random_number(9999),
-        #destination_account_id: SecureRandom.random_number(9999),
-        #amount: 50000
